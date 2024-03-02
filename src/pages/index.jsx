09cef4513fb34a7240eb9a30e71d07ba4a23a939
Gentil Alyson <fernandes.gentilalysonjac@gmail.com>
2024-03-02 15:10:20 -0300

    chore: removes clutter from home

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
@@ -14,9 +13,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <h1>Hello World</h1>
-      </main>
+      <body>
+        <main>
+          <h1>Hello World</h1>
+        </main>
+      </body>
     </>
   );
 }
