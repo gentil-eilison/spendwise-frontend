68c52b0fcd1a0ccec75a92b8e2d36ab50a31b55e
Gentil Alyson <fernandes.gentilalysonjac@gmail.com>
2024-03-02 15:07:31 -0300

    chore: initial commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>SpendWise</title>
+        <meta name="description" content="Spend your money in wiser way" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${styles.main} ${inter.className}`}>
+        <h1>Hello World</h1>
+      </main>
+    </>
+  );
+}
