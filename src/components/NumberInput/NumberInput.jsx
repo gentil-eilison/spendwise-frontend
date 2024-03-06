2b3c53ba3441fe0107fff8956b26a4bcd50d891f
Gentil Alyson <fernandes.gentilalysonjac@gmail.com>
2024-03-06 16:58:44 -0300

    chore: line breaks on NumberInput.jsx

--- src/components/NumberInput/NumberInput.jsx
+++ src/components/NumberInput/NumberInput.jsx
@@ -2,7 +2,11 @@ export default function NumberInput({ name, label, onChange, placeholder, step, 
     return (
         <div>
             <label className="form-label fw-bold" htmlFor={ name }>{ label }</label>
-            <input value={value} step={step} placeholder={ placeholder } onChange={ onChange } name={ name } id={ name } className="form-control" type="number" />
+            <input 
+                value={value} step={step} placeholder={ placeholder } 
+                onChange={ onChange } name={ name } id={ name } 
+                className="form-control" type="number" 
+            />
         </div>
     )
 }
