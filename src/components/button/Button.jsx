4432ee174c9a42704de53074bf1f119a3f00ebe0
Gentil Alyson <fernandes.gentilalysonjac@gmail.com>
2024-03-06 16:56:34 -0300

    chore: line breaks again

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -2,6 +2,10 @@ import style from "@/components/button/Button.module.css"
 
 export default function Button({ type, color, children, id, onClick }) {
     return (
-        <button onClick={onClick} id={id} type={type} className={`btn ${color} ${style.customButton}`}>{ children }</button>
+        <button 
+            onClick={onClick} id={id} type={type} 
+            className={`btn ${color} ${style.customButton}`}>
+                { children }
+        </button>
     )
 }
