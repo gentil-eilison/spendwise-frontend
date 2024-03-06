da6abe7f8366392dc3dad41034bee120e82ffa70
Gentil Alyson <fernandes.gentilalysonjac@gmail.com>
2024-03-06 16:58:10 -0300

    chore: line breaks on Input.jsx

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -2,7 +2,11 @@ export default function Input({ type, name, label, onChange, placeholder, value 
     return (
         <div>
             <label className="form-label fw-bold" htmlFor={ name }>{ label }</label>
-            <input value={value} placeholder={ placeholder } onChange={ onChange } name={ name } id={ name } className="form-control" type={type} />
+            <input 
+                value={value} placeholder={ placeholder } 
+                onChange={ onChange } name={ name } id={ name } 
+                className="form-control" type={type} 
+            />
         </div>
     )
 }
